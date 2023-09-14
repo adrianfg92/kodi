1f0f406bb488a7b31e9a90e74ab49af5b5f3f260
Adrián <adrian.fernandez@anjanadata.com>
2023-09-15 01:11:26 +0200

    Update

--- build.sh
+++ build.sh
@@ -2,24 +2,24 @@ echo "Init build...";
 
 echo " 1/6 ==> Removing old builds folders";
 rm -R build;
-rm -R adrian;
+rm -R repo;
 
 echo " 2/6 ==> Generate files";
-./create_repository.py --datadir=build https://github.com/adrianfg92/kodi.backups ./repository;
+./create_repository.py --datadir=build https://github.com/repofg92/kodi.backups ./repository;
 
-echo " 3/6 ==> Create \"adrian\" folder";
-mkdir adrian;
+echo " 3/6 ==> Create \"repo\" folder";
+mkdir repo;
 
 echo " 4/6 ==> Copy all zip files";
-cp -r build/**/*.zip adrian;
+cp -r build/**/*.zip repo;
 
 echo " 5/6 ==> Creating index.html";
 
-zips=`ls adrian`
-echo '<!DOCTYPE html>' > adrian/index.html;
+zips=`ls repo`
+echo '<!DOCTYPE html>' > repo/index.html;
 for name_zip in $zips
 do
-  echo "<a href=\"$name_zip\">$name_zip</a>" >> adrian/index.html;
+  echo "<a href=\"$name_zip\">$name_zip</a>" >> repo/index.html;
 done
 
 echo " 6/6 ==> Commit and push";
